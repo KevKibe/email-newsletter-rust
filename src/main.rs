13869e7369b39deab39513d58b8ef5aa7c7c3bd1
KevKibe <kchegz234@gmail.com>
2024-04-13 00:34:36 +0300

    refactor code structure, modify main fn to be public

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use email_newsletter::run;
+use email_newsletter::startup::run;
+use std::net::TcpListener;
+use email_newsletter::configuration::get_configuration;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    run()?.await
+pub async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
+
