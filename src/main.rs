556d565b8d0121fe3b7c4b58ca4ce6593394d6bf
KevKibe <kchegz234@gmail.com>
2024-04-16 22:14:56 +0300

    update connection to use PgPool connection, connection parameter in startup function.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use email_newsletter::startup::run;
 use std::net::TcpListener;
 use email_newsletter::configuration::get_configuration;
+use sqlx::PgPool;
 
 #[tokio::main]
-pub async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
 
