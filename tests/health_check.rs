add9fe995cfa59b055d0b479103f7870f4785c1b
KevKibe <kchegz234@gmail.com>
2024-04-16 22:13:27 +0300

    update spawn app, to use new parameters in startup function.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,13 +2,22 @@ use std::net::TcpListener;
 use sqlx::{PgConnection, Connection};
 use email_newsletter::configuration::get_configuration;
 
-fn spawn_app() -> String {
+async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
-        let port = listener.local_addr().unwrap().port();
-    let server = email_newsletter::startup::run(listener).expect("Failed to bind address");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127:0.0.1:{}", port);
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let server = run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+        }
 }
 
 
